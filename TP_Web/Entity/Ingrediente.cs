de76fb8a544e09e625c8da72ff9c1e8e529b8c1b
JeielLovera <jeiel.disas@gmail.com>
2019-04-18 21:42:49 -0500

    Inicio del proyecto

---
+++ TP_Web/Entity/Ingrediente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Ingrediente
+    {
+        public int idIngrediente { get; set; }
+        public String NIngrediente { get; set; }
+        public float QUnidadMedidaIngrediente { get; set; }
+        public String NUnidadMedidaIngrediente { get; set; }
+        public int TipoIngrediente { get; set; }
+
+
+    }
+}
