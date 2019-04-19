1e7537e022a25061051c5503f39c2786957d5002
JeielLovera <jeiel.disas@gmail.com>
2019-04-19 00:57:17 -0500

    implementacion CRUD de TipoProducto y TipoIngrediente

---
+++ TP_Web/Data/Implementation/TipoIngredienteRepository.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Entity;
+using System.Configuration;
+
+namespace Data.Implementation
+{
+    public class TipoIngredienteRepository : ITipoIngredienteRepository
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoIngrediente> FindAll()
+        {
+            var tipos = new List<TipoIngrediente>();
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select*from TipoIngrediente", con);
+                    using (var dr=cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            var tpingrediente = new TipoIngrediente();
+                            tpingrediente.CTipoIngrediente = Convert.ToInt32(dr["CTipoIngrediente"]);
+                            tpingrediente.NTipoIngrediente = dr["NTipoIngrediente"].ToString();
+                            tipos.Add(tpingrediente);
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return tipos;
+        }
+
+        public TipoIngrediente FindById(int? id)
+        {
+            TipoIngrediente tpingrediente = null;
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select*from TipoIngrediente where CTipoIngrediente='" + id + "'", con);
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            tpingrediente = new TipoIngrediente();
+                            tpingrediente.CTipoIngrediente = Convert.ToInt32(dr["CTipoIngrediente"]);
+                            tpingrediente.NTipoIngrediente = dr["NTipoIngrediente"].ToString();
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return tpingrediente;
+        }
+
+        public bool Insert(TipoIngrediente t)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("insert into TipoIngrediente values(@nombre)", con);
+                    cmd.Parameters.AddWithValue("@nombre", t.NTipoIngrediente);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    rpta = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rpta;
+        }
+
+        public bool Update(TipoIngrediente t)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("update TipoIngrediente set NTipoIngrediente=@nombre where CTipoIngrediente=@id", con);
+                    cmd.Parameters.AddWithValue("@id", t.CTipoIngrediente);
+                    cmd.Parameters.AddWithValue("@nombre", t.NTipoIngrediente);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    rpta = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return rpta;
+        }
+    }
+}
---
+++ TP_Web/Data/Implementation/TipoProductoRepository.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Entity;
+using System.Configuration;
+
+namespace Data.Implementation
+{
+    public class TipoProductoRepository : ITipoProductoRepository
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoProducto> FindAll()
+        {
+            var tipos = new List<TipoProducto>();
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select*from TipoProducto", con);
+                    using (var dr=cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            var tpproducto = new TipoProducto();
+                            tpproducto.CTipoProducto = Convert.ToInt32(dr["CTipoProducto"]);
+                            tpproducto.NTipoProducto = dr["NTipoProducto"].ToString();
+                            tpproducto.CSubTipoProducto = Convert.ToInt32(dr["CSubTipoProducto"]);
+                            tipos.Add(tpproducto);
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return tipos;
+
+        }
+
+        public TipoProducto FindById(int? id)
+        {
+            TipoProducto tpproducto = null;
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString())) 
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select*from TipoProducto where CTipoProducto='" + id + "'", con);
+                    using (var dr=cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            tpproducto = new TipoProducto();
+                            tpproducto.CTipoProducto = Convert.ToInt32(dr["CTipoProducto"]);
+                            tpproducto.NTipoProducto = dr["NTipoProducto"].ToString();
+                            tpproducto.CSubTipoProducto = Convert.ToInt32(dr["CSubTipoProducto"]);
+                        }
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return tpproducto;
+        }
+
+        public bool Insert(TipoProducto t)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("insert into TipoProducto values(@nombre,@subid)", con);
+                    cmd.Parameters.AddWithValue("@nombre", t.NTipoProducto);
+                    cmd.Parameters.AddWithValue("@subid", t.CSubTipoProducto);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rpta;
+        }
+
+        public bool Update(TipoProducto t)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("update TipoProducto set NTipoProducto=@nombre, CSubTipoProducto=@subid where CTipoProducto=@id", con);
+                    cmd.Parameters.AddWithValue("@id", t.CTipoProducto);
+                    cmd.Parameters.AddWithValue("@nombre", t.NTipoProducto);
+                    cmd.Parameters.AddWithValue("@subid", t.CSubTipoProducto);
+                    cmd.ExecuteNonQuery();
+                    rpta = true;
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rpta;
+        }
+    }
+}
