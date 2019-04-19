7059824518f120ae935d325d406ae48b974ee078
JeielLovera <jeiel.disas@gmail.com>
2019-04-19 00:58:43 -0500

    implementacion CRUD de TipoProducto y TipoIngrediente en Business

---
+++ TP_Web/Business/Implementation/TipoIngredienteService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    public class TipoIngredienteService : ITipoIngredienteService
+    {
+        private ITipoIngredienteRepository tpingredienteRepository = new TipoIngredienteRepository();
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoIngrediente> FindAll()
+        {
+            return tpingredienteRepository.FindAll();
+        }
+
+        public TipoIngrediente FindById(int? id)
+        {
+            return tpingredienteRepository.FindById(id);
+        }
+
+        public bool Insert(TipoIngrediente t)
+        {
+            return tpingredienteRepository.Insert(t);
+        }
+
+        public bool Update(TipoIngrediente t)
+        {
+            return tpingredienteRepository.Update(t);
+        }
+    }
+}
---
+++ TP_Web/Business/Implementation/TipoProductoService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    public class TipoProductoService : ITipoProductoService
+    {
+        private ITipoProductoRepository tpproductoRepository = new TipoProductoRepository();
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TipoProducto> FindAll()
+        {
+            return tpproductoRepository.FindAll();
+        }
+
+        public TipoProducto FindById(int? id)
+        {
+            return tpproductoRepository.FindById(id);
+        }
+
+        public bool Insert(TipoProducto t)
+        {
+            return tpproductoRepository.Insert(t);
+        }
+
+        public bool Update(TipoProducto t)
+        {
+            return tpproductoRepository.Update(t);
+        }
+    }
+}
