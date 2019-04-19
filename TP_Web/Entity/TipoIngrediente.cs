503bd41f844674a7b8e53c31bef2e25ceadcefc5
JeielLovera <jeiel.disas@gmail.com>
2019-04-19 00:53:48 -0500

    creacion de la entidad TipoIngrediente

---
+++ TP_Web/Entity/TipoIngrediente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class TipoIngrediente
+    {
+        public int CTipoIngrediente { get; set; }
+        public String NTipoIngrediente { get; set; }
+    }
+}
