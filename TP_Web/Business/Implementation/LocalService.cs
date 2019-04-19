b13359b89c8817f101e7b8a9e67b1175a0e263b7
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-19 01:15:21 -0500

    Add files via upload

---
+++ TP_Web/Business/Implementation/LocalService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    public class LocalService : ILocalService
+    {
+        private ILocalRepository LocalRepository= new LocalRepository();
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Local> FindAll()
+        {
+            return LocalRepository.FindAll();
+        }
+
+        public Local FindById(int? id)
+        {
+            return LocalRepository.FindById(id);
+        }
+
+        public bool Insert(Local t)
+        {
+            return LocalRepository.Insert(t);
+        }
+
+        public bool Update(Local t)
+        {
+            return LocalRepository.Update(t);
+        }
+    }
+}
---
+++ TP_Web/Business/Implementation/RolService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+using Data;
+using Data.Implementation;
+
+namespace Business.Implementation
+{
+    public class RolService : IRolService
+    {
+        private IRolRepository RolRepository = new RolRepository();
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Rol> FindAll()
+        {
+            return RolRepository.FindAll();
+        }
+
+        public Rol FindById(int? id)
+        {
+            return RolRepository.FindById(id);
+        }
+
+        public bool Insert(Rol t)
+        {
+            return RolRepository.Insert(t);
+        }
+
+        public bool Update(Rol t)
+        {
+            return RolRepository.Update(t);
+        }
+    }
+}
