ae808794e91fac215661d419415952ede2945e1d
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-19 22:50:57 -0500

    interface Mesa Repository

---
+++ TP_Web/Data/IMesaRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace Data
+{
+    public interface IMesaRepository:ICrudRepository<Mesa>
+    {
+    }
+}
