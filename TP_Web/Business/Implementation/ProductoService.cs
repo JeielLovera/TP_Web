9dfb11aa2e5c070581664d8981b3672deeeccb5e
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-19 21:23:31 -0500

    Implementacion Producto Service

---
+++ TP_Web/Business/Implementation/ProductoService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    public class ProductoService : IProductoService
+    {
+        private IProductoRepository ProductoRepository = new ProductoRepository();
+        private ITipoProductoRepository tipoProductoRepository = new TipoProductoRepository();
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Producto> FindAll()
+        {
+            return ProductoRepository.FindAll();
+        }
+
+        public Producto FindById(int? id)
+        {
+            return ProductoRepository.FindById(id);
+        }
+
+        public bool Insert(Producto t)
+        {
+            TipoProducto tipoproducto = tipoProductoRepository.FindById(t.CTipoProducto.CTipoProducto);
+            t.CTipoProducto = tipoproducto;
+
+            return ProductoRepository.Insert(t);
+        }
+
+        public bool Update(Producto t)
+        {
+            return ProductoRepository.Update(t);
+        }
+    }
+}
