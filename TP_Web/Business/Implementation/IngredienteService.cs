0cef5c175cf962386ef935cdde08b5521f5fe290
JeielLovera <jeiel.disas@gmail.com>
2019-04-19 21:58:00 -0500

    creacion de la entidad Ingrediente
    interfases e implementacion de metodos de la entidad Ingrediente en Data y Business

---
+++ TP_Web/Business/Implementation/IngredienteService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    class IngredienteService : IIngredienteService
+    {
+        private IIngredienteRepository ingredienteRepository = new IngredienteRepository();
+        private ITipoIngredienteRepository tpingredienteRepository = new TipoIngredienteRepository();
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Ingrediente> FindAll()
+        {
+            return ingredienteRepository.FindAll();
+        }
+
+        public Ingrediente FindById(int? id)
+        {
+            return ingredienteRepository.FindById(id);
+
+        }
+
+        public bool Insert(Ingrediente t)
+        {
+            TipoIngrediente tpingrediente = tpingredienteRepository.FindById(t.CTipoIngrediente.CTipoIngrediente);
+            t.CTipoIngrediente = tpingrediente;
+            return ingredienteRepository.Insert(t);
+
+        }
+
+        public bool Update(Ingrediente t)
+        {
+            TipoIngrediente tpingrediente = tpingredienteRepository.FindById(t.CTipoIngrediente.CTipoIngrediente);
+            t.CTipoIngrediente = tpingrediente;
+            return ingredienteRepository.Update(t);
+        }
+    }
+}
---
+++ TP_Web/Data/Implementation/IngredienteRepository.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using Entity;
+using System.Configuration;
+
+namespace Data.Implementation
+{
+    public class IngredienteRepository : IIngredienteRepository
+    {
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Ingrediente> FindAll()
+        {
+            var ingredientes = new List<Ingrediente>();
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select ig.CIngrediente, ig.NIngrediente, ig.QUnidadMedidaIngrediente, ig.NUnidadMedidaIngrediente, tp.NTipoIngrediente as TipoIngrediente from Ingrediente ig, TipoIngrediente tp where ig.CTipoIngrediente=tp.CTipoIngrediente", con);
+                    using (var dr=cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            var ingrediente = new Ingrediente();
+                            var tipo = new TipoIngrediente();
+                            ingrediente.CIngrediente = Convert.ToInt32(dr["ig.CIngrediente"]);
+                            ingrediente.NIngrediente = dr["ig.NIngrediente"].ToString();
+                            ingrediente.QUnidadMedidaIngrediente= Convert.ToInt32(dr["ig.QUnidadMedidaIngrediente"]);
+                            ingrediente.NUnidadMedidaIngrediente = dr["ig.NUnidadMedidaIngrediente"].ToString();
+                            tipo.NTipoIngrediente = dr["TipoIngrediente"].ToString();
+                            ingrediente.CTipoIngrediente = tipo;
+                            ingredientes.Add(ingrediente);                   
+                        }
+
+                    }
+                    con.Close();
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return ingredientes;
+        }
+
+        public Ingrediente FindById(int? id)
+        {
+            Ingrediente ingrediente = null;
+            try
+            {
+                using (var con = new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("select ig.CIngrediente, ig.NIngrediente, ig.QUnidadMedidaIngrediente, ig.NUnidadMedidaIngrediente, tp.NTipoIngrediente as TipoIngrediente from Ingrediente ig, TipoIngrediente tp where ig.CTipoIngrediente='"+id+"'", con);
+                    using (var dr = cmd.ExecuteReader())
+                    {
+                        ingrediente = new Ingrediente();
+                        var tipo = new TipoIngrediente();
+                        ingrediente.CIngrediente = Convert.ToInt32(dr["ig.CIngrediente"]);
+                        ingrediente.NIngrediente = dr["ig.NIngrediente"].ToString();
+                        ingrediente.QUnidadMedidaIngrediente = Convert.ToInt32(dr["ig.QUnidadMedidaIngrediente"]);
+                        ingrediente.NUnidadMedidaIngrediente = dr["ig.NUnidadMedidaIngrediente"].ToString();
+                        tipo.NTipoIngrediente = dr["TipoIngrediente"].ToString();
+                        ingrediente.CTipoIngrediente = tipo;
+                        
+                    }
+                }
+
+            }
+            catch(Exception)
+            {
+                throw;
+            }
+            return ingrediente;
+            
+        }
+
+        public bool Insert(Ingrediente t)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("insert into Ingrediente values (@nombre, @qunidadmedida, @nunidadmedida,@ctipo)", con);
+                    cmd.Parameters.AddWithValue("@nombre", t.NIngrediente);
+                    cmd.Parameters.AddWithValue("@qunidadmedida", t.QUnidadMedidaIngrediente);
+                    cmd.Parameters.AddWithValue("@nunidadmedida", t.NUnidadMedidaIngrediente);
+                    cmd.Parameters.AddWithValue("@ctipo", t.CTipoIngrediente);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    rpta = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rpta;
+        }
+
+        public bool Update(Ingrediente t)
+        {
+            bool rpta = false;
+            try
+            {
+                using (var con=new SqlConnection(ConfigurationManager.ConnectionStrings["BD_Pizza"].ToString()))
+                {
+                    con.Open();
+                    var cmd = new SqlCommand("update Ingrediente set NIngrediente=@nombre, QUnidadMedidaIngrediente=@qunidadmedida, NUnidadMedidaIngrediente=@nunidadmedida, CTipoIngrediente=@ctipo where CIngrediente=@id", con);
+                    cmd.Parameters.AddWithValue("@id", t.CIngrediente);
+                    cmd.Parameters.AddWithValue("@qunidadmedida", t.QUnidadMedidaIngrediente);
+                    cmd.Parameters.AddWithValue("@nunidadmedida", t.NUnidadMedidaIngrediente);
+                    cmd.Parameters.AddWithValue("@ctipo", t.CTipoIngrediente);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    rpta = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            return rpta;
+        }
+    }
+}
