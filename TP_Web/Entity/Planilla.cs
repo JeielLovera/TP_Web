35937853f5952e9ff4ab8cb675360e364665365b
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-20 12:45:13 -0500

    Planilla Entity

---
+++ TP_Web/Entity/Planilla.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entity;
+namespace Entity
+{
+    public class Planilla
+    {
+        public Empleado CEmpleado { get; set; }
+        public DateTime DFechaHRol { get; set; }
+        public Rol CRol { get; set; }
+        public float MSueldo { get; set; }
+    }
+}
