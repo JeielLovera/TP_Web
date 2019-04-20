3c16a0e74f319e5216086ea154b708dcfa3c9ed1
JeielLovera <jeiel.disas@gmail.com>
2019-04-20 06:19:31 -0500

    creacion de la entidad Historial_Jefe
    interfases e implementacion de la entidad Historial_Jefe en Data y Business

---
+++ TP_Web/Business/Implementation/Historial_JefeService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    public class Historial_JefeService : IHistorial_JefeService
+    {
+        private IHistorial_JefeRepository historialRepository = new Historial_JefeRepository();
+        private IEmpleadoRepository empleadoRepository = new EmpleadoRepository();
+        private IEmpleadoRepository jefeRepository = new EmpleadoRepository();
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Historial_Jefe> FindAll()
+        {
+            return historialRepository.FindAll();
+        }
+
+        public Historial_Jefe FindById(int? id)
+        {
+            return historialRepository.FindById(id);
+        }
+
+        public bool Insert(Historial_Jefe t)
+        {
+            Empleado empleado = empleadoRepository.FindById(t.CEmpleado.CEmpleado);
+            Empleado jefe = jefeRepository.FindById(t.CJefe.CEmpleado);
+            t.CEmpleado = empleado;
+            t.CJefe = jefe;
+            return historialRepository.Insert(t);
+        }
+
+        public bool Update(Historial_Jefe t)
+        {
+            Empleado empleado = empleadoRepository.FindById(t.CEmpleado.CEmpleado);
+            Empleado jefe = jefeRepository.FindById(t.CJefe.CEmpleado);
+            t.CEmpleado = empleado;
+            t.CJefe = jefe;
+            return historialRepository.Update(t);
+        }
+    }
+}
---
+++ TP_Web/Entity/Historial_Jefe.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Historial_Jefe
+    {
+        public Empleado CEmpleado { get; set; }
+        public DateTime DFechaHJefe { get; set; }
+        public Empleado CJefe { get; set; }
+    }
+}
