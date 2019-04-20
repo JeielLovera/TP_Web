cdc67c80467f493d9e4bd52ab825d943bdd34dda
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-19 22:50:28 -0500

    Mesa Entity

---
+++ TP_Web/Entity/Mesa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Mesa
+    {
+        public int CMesa { get; set; }
+        public int NumMesa { get; set; }
+        public int QCapacidad { get; set; }
+        public Local CLocal { get; set; }
+    }
+}
