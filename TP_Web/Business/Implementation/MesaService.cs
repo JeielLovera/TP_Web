fdb4936446d64fe2b4109a7df5030c61fa40745e
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-19 22:52:45 -0500

    Implementacion Mesa business

---
+++ TP_Web/Business/Implementation/MesaService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+using Data.Implementation;
+using Entity;
+
+namespace Business.Implementation
+{
+    public class MesaService : IMesaService
+    {
+        private IMesaRepository mesaRepository = new MesaRepository();
+        private ILocalRepository localRepository = new LocalRepository();
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Mesa> FindAll()
+        {
+            return mesaRepository.FindAll();
+        }
+
+        public Mesa FindById(int? id)
+        {
+            return mesaRepository.FindById(id);
+        }
+
+        public bool Insert(Mesa t)
+        {
+            Local local = localRepository.FindById(t.CLocal.CLocal);
+            t.CLocal = local;
+
+            return mesaRepository.Insert(t);
+        }
+
+        public bool Update(Mesa t)
+        {
+            return mesaRepository.Update(t);
+        }
+    }
+}
