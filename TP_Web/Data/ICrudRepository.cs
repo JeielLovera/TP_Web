4d4a48b038493a21210b6eee9fa0ec65e4ed8e41
JeielLovera <jeiel.disas@gmail.com>
2019-04-20 21:32:10 -0500

    update del las interdases CRUD en Data Y Business

--- TP_Web/Data/ICrudRepository.cs
+++ TP_Web/Data/ICrudRepository.cs
@@ -13,5 +13,7 @@ namespace Data
         bool Delete(int id);
         List<T> FindAll();
         T FindById(int? id);
+        T FindById(int? id, int? id2);
+        T FindById(int? id, int? id2, int? id3);
     }
 }
