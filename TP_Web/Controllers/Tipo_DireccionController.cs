d7d63b70ce7a1cf9ab0d1eee4daab9fe043e8bd4
JeielLovera <jeiel.disas@gmail.com>
2019-04-24 04:44:35 -0500

    creacion del controller y la vista Tipo_Direccion

---
+++ TP_Web/Controllers/Tipo_DireccionController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business;
+using Business.Implementation;
+using Entity;
+
+namespace TP_PIZZA.Controllers
+{
+    public class Tipo_DireccionController : Controller
+    {
+        private ITipo_DireccionService tpdireccionService = new Tipo_DireccionService();
+
+        // GET: Tipo_Direccion
+        public ActionResult Index()
+        {
+            return View(tpdireccionService.FindAll());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Tipo_Direccion tpdirec)
+        {
+            bool rpta = tpdireccionService.Insert(tpdirec);
+            if (rpta)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
