7b41879401a8b0d4e35842a1d1284d9083b483a2
JeielLovera <jeiel.disas@gmail.com>
2019-04-24 00:13:48 -0500

    creacion del controller Ingrediente y las vista Ingrediente

---
+++ TP_Web/Controllers/IngredienteController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business;
+using Business.Implementation;
+using Entity;
+
+namespace TP_PIZZA.Controllers
+{
+    public class IngredienteController : Controller
+    {
+        private IIngredienteService ingredienteService = new IngredienteService();
+        private ITipoIngredienteService tpingredienteService = new TipoIngredienteService();
+
+        // GET: Ingrediente
+        public ActionResult Index()
+        {
+            return View(ingredienteService.FindAll());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.tipoingrediente = tpingredienteService.FindAll();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Ingrediente ingrdnt)
+        {
+            ViewBag.tipoingrediente = tpingredienteService.FindAll();
+            bool rpta = ingredienteService.Insert(ingrdnt);
+            if (rpta)
+            {
+                return RedirectToAction("Index");
+
+            }
+            return View();
+        }
+    }
+}
