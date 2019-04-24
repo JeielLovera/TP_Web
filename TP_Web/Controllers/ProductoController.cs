78905fd54b35169bebf1b4f73b9b771b57ba722c
JeielLovera <jeiel.disas@gmail.com>
2019-04-24 04:05:57 -0500

    creacion del Controller Producto

---
+++ TP_Web/Controllers/ProductoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business;
+using Business.Implementation;
+using Entity;
+
+namespace TP_PIZZA.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private IProductoService productoService = new ProductoService();
+        // GET: Producto
+        public ActionResult Index()
+        {        
+           
+            return View(productoService.FindAll());
+        }
+    }
+}
