60196c28cb491c6f1b12245f4698607396be95e3
JeielLovera <jeiel.disas@gmail.com>
2019-04-28 04:06:10 -0500

    creacion de Producto_Ingrediente Controller

---
+++ TP_Web/Controllers/Producto_IngredienteController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business;
+using Business.Implementation;
+using Entity;
+
+namespace TP_PIZZA.Controllers
+{
+    public class Producto_IngredienteController : Controller
+    {
+        private IProducto_IngredienteService prodct_ingrdntService = new Producto_IngredienteService();
+        private IProductoService prodctService = new ProductoService();
+        private IIngredienteService ingrdntService = new IngredienteService();
+
+        // GET: Producto_Ingrediente
+        public ActionResult Index()
+        {
+            return View(prodct_ingrdntService.FindAll());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.producto = prodctService.FindAll();
+            ViewBag.ingrediente = ingrdntService.FindAll();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Producto_Ingrediente prodct_ingrdnt)
+        {
+            ViewBag.producto = prodctService.FindAll();
+            ViewBag.ingrediente = ingrdntService.FindAll();
+            bool rpta = prodct_ingrdntService.Insert(prodct_ingrdnt);
+            if (rpta)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
