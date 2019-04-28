49c700fa83073ef2533081f49fa0a5567232c711
GeraldineNisbeth <49768681+GeraldineNisbeth@users.noreply.github.com>
2019-04-28 00:35:55 -0500

    Create Producto y TipoProducto

--- TP_Web/Controllers/ProductoController.cs
+++ TP_Web/Controllers/ProductoController.cs
@@ -6,34 +6,54 @@ using System.Web.Mvc;
 using Business;
 using Business.Implementation;
 using Entity;
-
 namespace TP_PIZZA.Controllers
 {
     public class ProductoController : Controller
     {
-        private IProductoService productoService = new ProductoService();
-        private ITipoProductoService tpproductoService = new TipoProductoService();
-        
+        private IProductoService productoservice = new ProductoService();
+        private ITipoProductoService tpproductoservice = new TipoProductoService();
         // GET: Producto
         public ActionResult Index()
         {
-            return View(productoService.FindAll());
+            return View(productoservice.FindAll());
         }
-
         public ActionResult Create()
         {
-            ViewBag.tipoproducto = tpproductoService.FindAll();
+            ViewBag.tpproducto = tpproductoservice.FindAll();
             return View();
         }
-        [HttpPost]
-        public ActionResult Create(Producto producto)
+        public ActionResult Edit(int? id)
         {
-            ViewBag.tipoproducto = productoService.FindAll();
-            bool rpta = productoService.Insert(producto);
-            if (rpta)
+            if (id == null) { return HttpNotFound(); }
+            ViewBag.tpproducto = tpproductoservice.FindAll();
+            Producto producto = productoservice.FindById(id);
+            return View(producto);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Producto prod)
+        {
+            ViewBag.tpproducto = tpproductoservice.FindAll();
+            bool rpta = productoservice.Insert(prod);
+            if(rpta)
             {
                 return RedirectToAction("Index");
+            }
+             
+            return View();
+        }
 
+        [HttpPost]
+        public ActionResult Edit(Producto prod)
+        {
+            if (!ModelState.IsValid) { return View(); }
+
+            ViewBag.tpproducto = tpproductoservice.FindAll();
+            bool rpta = productoservice.Update(prod);
+
+            if(rpta)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
---
+++ TP_Web/Controllers/TipoProductoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Business;
+using Business.Implementation;
+using Entity;
+namespace TP_PIZZA.Controllers
+{
+    public class TipoProductoController : Controller
+    {
+        private ITipoProductoService tipoproductoservice = new TipoProductoService();
+        private ITipoProductoService sbtipoproductoservice = new TipoProductoService();
+        // GET: TipoProducto
+        public ActionResult Index()
+        {
+            return View(tipoproductoservice.FindAll());
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.sbtpproducto = sbtipoproductoservice.FindAll();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(TipoProducto tp)
+        {
+            ViewBag.sbtpproducto = sbtipoproductoservice.FindAll();
+            bool rpta = tipoproductoservice.Insert(tp);
+            if(rpta)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
